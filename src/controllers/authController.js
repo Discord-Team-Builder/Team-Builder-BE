1833d1d6fa801daf8efb11c79fb33d8c04687384
utkarshya24 <utky24@gmail.com>
2025-05-26 21:59:00 +0000

    fix: update SameSite cookie attribute for production environment

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -103,7 +103,7 @@ export const discordCallback = async (req, res) => {
    res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',// Set to true in production
-    sameSite: 'Strict', 
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' :  'Strict', 
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
    
     // Convert to milliseconds
